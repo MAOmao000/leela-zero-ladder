f0cc4b8f819dae20e35ac966f15ad8f3408e297c
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2024-08-26 02:21:17 +0900

    Set the default batch size of the CUDNN LEGACY backend equal to the number of threads.

--- src/Leela.cpp
+++ src/Leela.cpp
@@ -111,8 +111,13 @@ static void calculate_thread_count_gpu(
         if (vm["batchsize"].as<unsigned int>() > 0) {
             cfg_batch_size = vm["batchsize"].as<unsigned int>();
         } else {
-            cfg_batch_size =
-                (cfg_num_threads + (gpu_count * 2) - 1) / (gpu_count * 2);
+            if (cfg_backend == backend_t::CUDNN) {
+                cfg_batch_size =
+                    (cfg_num_threads + (gpu_count * 1) - 1) / (gpu_count * 1);
+            } else {
+                cfg_batch_size =
+                    (cfg_num_threads + (gpu_count * 2) - 1) / (gpu_count * 2);
+            }
 
             // no idea why somebody wants to use threads less than the number of GPUs
             // but should at least prevent crashing
@@ -125,11 +130,20 @@ static void calculate_thread_count_gpu(
             cfg_batch_size = vm["batchsize"].as<unsigned int>();
         } else {
             calculate_thread_count_cpu(vm);
+            if (cfg_backend == backend_t::CUDNN) {
+                cfg_batch_size = cfg_num_threads * 5 / 3;
+            } else {
                 cfg_batch_size = cfg_num_threads * 5 / 6;
+            }
         }
 
-        cfg_num_threads =
-            std::min(cfg_max_threads, cfg_batch_size * gpu_count * 2);
+        if (cfg_backend == backend_t::CUDNN) {
+            cfg_num_threads =
+                std::min(cfg_max_threads, cfg_batch_size * gpu_count * 1);
+        } else {
+            cfg_num_threads =
+                std::min(cfg_max_threads, cfg_batch_size * gpu_count * 2);
+        }
     }
     if (cfg_num_threads < cfg_batch_size) {
         printf(
