ecf37c2b42b24d75aecbc2f14c6168f729aca2c1
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2024-10-25 18:40:51 +0900

    Change the default number of threads and batch size.

--- src/Leela.cpp
+++ src/Leela.cpp
@@ -111,13 +111,12 @@ static void calculate_thread_count_gpu(
         if (vm["batchsize"].as<unsigned int>() > 0) {
             cfg_batch_size = vm["batchsize"].as<unsigned int>();
         } else {
-            if (cfg_backend == backend_t::CUDNNGRAPH
-                || cfg_backend == backend_t::CUDNN) {
-                cfg_batch_size =
-                    (cfg_num_threads + (gpu_count * 1) - 1) / (gpu_count * 1);
-            } else {
+            if (cfg_backend == backend_t::OPENCL) {
                 cfg_batch_size =
                     (cfg_num_threads + (gpu_count * 2) - 1) / (gpu_count * 2);
+            } else {
+                cfg_batch_size =
+                    (cfg_num_threads + (gpu_count * 1) - 1) / (gpu_count * 1);
             }
             // no idea why somebody wants to use threads less than the number of GPUs
             // but should at least prevent crashing
@@ -130,23 +129,17 @@ static void calculate_thread_count_gpu(
             cfg_batch_size = vm["batchsize"].as<unsigned int>();
         } else {
             calculate_thread_count_cpu(vm);
-            if (cfg_backend == backend_t::TENSORRT) {
-                cfg_batch_size = cfg_num_threads * 5 / 6;
-            } else {
+            if (cfg_backend == backend_t::OPENCL) {
                 cfg_batch_size = cfg_num_threads * 5 / 12;
+            } else {
+                cfg_batch_size = cfg_num_threads * 5 / 6;
             }
             if (cfg_batch_size == 0) {
                 cfg_batch_size = 1;
             }
         }
-
-        if (cfg_backend == backend_t::TENSORRT) {
-            cfg_num_threads =
-                std::min(cfg_max_threads, cfg_batch_size * gpu_count * 1);
-        } else {
-            cfg_num_threads =
-                std::min(cfg_max_threads, cfg_batch_size * gpu_count * 2);
-        }
+        cfg_num_threads =
+            std::min(cfg_max_threads, cfg_batch_size * gpu_count * 2);
     }
     if (cfg_num_threads < cfg_batch_size) {
         printf(
