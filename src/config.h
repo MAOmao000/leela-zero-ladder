6da1fa288c24bec561442c8eb1772d9600c4bb4b
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2024-12-28 11:03:07 +0900

    Change Leelazero version.

--- src/config.h
+++ src/config.h
@@ -126,7 +126,7 @@ enum class NetworkType {
 
 static constexpr auto PROGRAM_NAME = "Leela Zero(ladder detection)";
 static constexpr auto PROGRAM_VERSION_MAJOR = "2";
-static constexpr auto PROGRAM_VERSION_MINOR = "3";
+static constexpr auto PROGRAM_VERSION_MINOR = "4";
 
 /*
  * OpenBLAS limitation: the default configuration on some Linuxes
