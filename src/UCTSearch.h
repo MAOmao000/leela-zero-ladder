c04633b8e1fd9a7035cd4b33d90c1c9b4d8a3992
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-07-05 11:25:35 +0900

    Optionally output statistics for analysis.

--- src/UCTSearch.h
+++ src/UCTSearch.h
@@ -126,6 +126,9 @@ public:
     SearchResult play_simulation(GameState& currstate, UCTNode* node);
     bool have_alternate_moves(int elapsed_centis, int time_for_move);
     bool stop_thinking(int elapsed_centis = 0, int time_for_move = 0) const;
+    void set_analysis_out(bool analysis_out) {
+        m_analysis_out = analysis_out;
+    }
 
 private:
     float get_min_psa_ratio() const;
@@ -162,6 +165,7 @@ private:
     std::mutex m_mutex_stop;
     std::condition_variable m_cv;
     std::condition_variable m_cv_analysis_stop;
+    bool m_analysis_out{true};
     bool m_analysis_stop{false};
     std::thread m_analysis;
 };
@@ -169,27 +173,18 @@ private:
 class UCTWorker {
 public:
     UCTWorker(GameState& state, UCTSearch* const search, UCTNode* const root,
-#ifdef USE_OPENCL
-        Network& network,
-#endif
-        Time* const start = nullptr, int const time_for_move = 0)
-        : m_rootstate(state), m_search(search), m_root(root),
-            m_start(start), m_time_for_move(time_for_move)
-#ifdef USE_OPENCL
-            , m_network(network) 
-#endif
-        {}
+        Network& network, Time* const start = nullptr, int const time_for_move = 0)
+        : m_rootstate(state), m_search(search), m_root(root), m_network(network),
+            m_start(start), m_time_for_move(time_for_move) {}
     void operator()();
 
 private:
     GameState& m_rootstate;
     UCTSearch* m_search;
     UCTNode* m_root;
+    Network& m_network;
     Time* m_start;
     int m_time_for_move;
-#ifdef USE_OPENCL
-    Network& m_network;
-#endif
 };
 
 #endif
