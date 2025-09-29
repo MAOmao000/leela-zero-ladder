f3a9bbb6f8d9f52e8b4988b83837674fdcc316b0
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-09-30 00:43:23 +0900

    Addressed cmake warning.

--- src/config.h
+++ src/config.h
@@ -128,7 +128,7 @@ enum class NetworkType {
 static constexpr auto PROGRAM_NAME = "Leela Zero(ladder detection)";
 static constexpr auto PROGRAM_VERSION_MAJOR = "3";
 static constexpr auto PROGRAM_VERSION_MINOR = "0";
-static constexpr auto PROGRAM_VERSION_PATCH = "10";
+static constexpr auto PROGRAM_VERSION_PATCH = "11";
 
 /*
  * OpenBLAS limitation: the default configuration on some Linuxes
